12e86bbc1d48b1c4a7df4f0b5bec0ce44f98630d
Tyler Briskie <tylerbriskie@gmail.com>
2016-11-14 13:33:56 -0700

    began work on song request from API

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
 
 $(".button-collapse").sideNav();
 $(".card-action:a").click();
+
+$('#songSubmit').click(function(){
+  var $songA = $('input[name=song_a]').val();
+  var $songB = $('input[name=song_b]').val();
+  console.log($songA,$songB);
+
+});
+
 });
