73374bbf75a78e6f76b316d4995437dc6129b714
Tyler Briskie <tylerbriskie@gmail.com>
2016-11-14 16:35:52 -0700

    working songs results page, needs refinement

--- script.js
+++ script.js
@@ -6,8 +6,60 @@ $(".card-action:a").click();
 $('#songSubmit').click(function(){
   var $songA = $('input[name=song_a]').val();
   var $songB = $('input[name=song_b]').val();
-  console.log($songA,$songB);
-
+  var $songA_regex = $songA.replace(/\W+/g, '-').toLowerCase();
+  var $songB_regex = $songB.replace(/\W+/g, '-').toLowerCase();
+  $.get('http://phish.in/api/v1/songs/'+ $songA_regex, displaySongAInfo, "json");
+  $.get('http://phish.in/api/v1/songs/'+ $songB_regex, displaySongBInfo, "json");
 });
 
+function displaySongAInfo(song){
+  var details = {};
+  var tracks = song.data.tracks
+  details.timesPlayed = song.data.tracks_count;
+  details.debut = song.data.tracks[0].show_date;
+  details.mostRecent = song.data.tracks[tracks.length-1].show_date;
+  $('.song-a-details').append('<h5>Times Played</h5>');
+  $('.song-a-details').append('<p>' + details.timesPlayed + '</p>');
+  $('.song-a-details').append('<h5>First Played</h5>');
+  $('.song-a-details').append('<p>' + details.debut + '</p>');
+  $('.song-a-details').append('<h5>Most Recently Played</h5>');
+  $('.song-a-details').append('<p>' + details.mostRecent + '</p>');
+};
+function displaySongBInfo(song){
+  var details = {};
+  var tracks = song.data.tracks
+  details.timesPlayed = song.data.tracks_count;
+  details.debut = song.data.tracks[0].show_date;
+  details.mostRecent = song.data.tracks[tracks.length-1].show_date;
+  $('.song-b-details').append('<h5>Times Played</h5>');
+  $('.song-b-details').append('<p>' + details.timesPlayed + '</p>');
+  $('.song-b-details').append('<h5>First Played</h5>');
+  $('.song-b-details').append('<p>' + details.debut + '</p>');
+  $('.song-b-details').append('<h5>Most Recently Played</h5>');
+  $('.song-b-details').append('<p>' + details.mostRecent + '</p>');
+};
+
+
+
+// function displayVenueInfo(venue){
+//
+// };
+//
+// function getSongInfo(song){
+//   var myGet = new XMLHttpRequest();
+//   myGet.open('GET', 'http://phish.in/api/v1/songs/'+ song +'.json', true);
+//   myGet.responseType='json';
+//   myGet.setRequestHeader('Accept', 'application/json');
+//   myGet.addEventListener("load", displaySongInfo);
+//   myGet.send();
+//
+// };
+//
+// function getSongInfoJQuery(song){
+//   $.get('http://phish.in/api/v1/songs/'+ song, displaySongInfo);
+// };
+// function getVenueInfo(venue){
+//
+// };
+//
 });
