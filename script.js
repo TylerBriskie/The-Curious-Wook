d9c9234fdab6346573933486b3d6f9bf3bc4fd28
Tyler Briskie <tylerbriskie@gmail.com>
2016-11-16 16:34:07 -0700

    proxy server working

--- script.js
+++ script.js
@@ -8,26 +8,17 @@ $(".card-action:a").click();
 
 //  ====submit API requests====
 $('#songSubmit').click(function(){
-  event.preventDefault();
+  // event.preventDefault();
   var $songA = $('input[name=song_a]').val();
   var $songB = $('input[name=song_b]').val();
   var $songA_regex = $songA.replace(/\W+/g, '-').toLowerCase();
   var $songB_regex = $songB.replace(/\W+/g, '-').toLowerCase();
-//   $.ajax({
-//
-//     url: 'http://phish.in/api/v1/songs/'+ $songA_regex,
-//     data: song,
-//     type: 'GET',
-//     crossDomain: true,
-//     dataType: 'jsonp',
-//     success: displaySongAInfo(),
-//     beforeSend: setHeader
-// });
   $.get('https://galvanize-cors-proxy.herokuapp.com/http://phish.in/api/v1/songs/'+ $songA_regex, displaySongAInfo, "json");
   $.get('https://galvanize-cors-proxy.herokuapp.com/http://phish.in/api/v1/songs/'+ $songB_regex, displaySongBInfo, "json");
 });
 
 $('#venueSubmit').click(function(){
+  event.preventDefault;
   var $venueA = $('input[name=venue_a]').val();
   var $venueB = $('input[name=venue_b]').val();
   var $venueA_regex = $venueA.replace(/\W+/g, '-').toLowerCase();
@@ -35,6 +26,8 @@ $('#venueSubmit').click(function(){
   $.get('https://galvanize-cors-proxy.herokuapp.com/http://phish.in/api/v1/venues/'+ $venueA_regex, displayVenueAInfo, "json");
   $.get('https://galvanize-cors-proxy.herokuapp.com/http://phish.in/api/v1/venues/'+ $venueB_regex, displayVenueBInfo, "json");
 });
+
+
 
 
 //clear forms
